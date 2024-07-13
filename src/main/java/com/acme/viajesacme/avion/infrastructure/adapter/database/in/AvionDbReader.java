cb3a156ac8d6da7d2753f72c1a4cdd0f2cde2712
SebastianGarciaLoyo <est.sloyo@smart.edu.co>
2024-07-13 03:39:35 -0500

    feat: :sparkles: Agregando la estructura base del proyecto Se agrega la respectiva estructura del proyecto aplicando los principios S.O.L.I.D, arquitectura hexagonal, vertical slicing y screaming architecture para asegurar un desacople total por característica, permitiéndolo hacer más robusto, independiente, seguro y escalable a futuras implementaciones de características

---
+++ src/main/java/com/acme/viajesacme/avion/infrastructure/adapter/database/in/AvionDbReader.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.acme.viajesacme.avion.infrastructure.adapter.database.in;
+
+/**
+ *
+ * @author camper
+ */
+public class AvionDbReader {
+    
+}
---
+++ src/main/java/com/acme/viajesacme/revision/infrastructure/adapter/console/in/RevisionConsoleCommandAdapter.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.acme.viajesacme.revision.infrastructure.adapter.console.in;
+
+/**
+ *
+ * @author camper
+ */
+public class RevisionConsoleCommandAdapter {
+    
+}
