dbb954333fe3593a050d43737b79752129f89021
DavMateo <davidcarrenomc@gmail.com>
2024-07-14 19:52:54 -0500

    feat: :art: Implementando la estructura base final La estructura ya está definida sobre la cuál se empezará a trabajar en el desarrollo de la aplicación

---
+++ src/main/java/com/acme/viajesacme/persona/infrastructure/adapter/login/out/PersonaLoginWriter.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.acme.viajesacme.persona.infrastructure.adapter.login.out;
+
+/**
+ *
+ * @author mateo
+ */
+public class PersonaLoginWriter {
+    
+}
